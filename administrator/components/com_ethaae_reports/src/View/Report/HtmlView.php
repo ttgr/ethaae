9374c8428f52067668e5e7059904220b858052b3
ttgr <tasos.tr@gmail.com>
2024-06-03 17:32:06 +0200

    Ethaae Reports: 1st full version of managing Reports

--- administrator/components/com_ethaae_reports/src/View/Report/HtmlView.php
+++ administrator/components/com_ethaae_reports/src/View/Report/HtmlView.php
@@ -106,18 +106,7 @@ class HtmlView extends BaseHtmlView
 			ToolbarHelper::save('report.save', 'JTOOLBAR_SAVE');
 		}
 
-		if (!$checkedOut && ($canDo->get('core.create')))
-		{
-			ToolbarHelper::custom('report.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
-		}
 
-		// If an existing item, can save to a copy.
-		if (!$isNew && $canDo->get('core.create'))
-		{
-			ToolbarHelper::custom('report.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
-		}
-
-		
 
 		if (empty($this->item->id))
 		{
